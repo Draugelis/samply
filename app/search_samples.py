37c5ab0d8e8258c05ce22c079ed7e1b9eefba293
Draugelis <github@pysc.eu>
2022-08-13 22:20:06 +0300

    add spotify track search to sample search.

--- app/search_samples.py
+++ app/search_samples.py
@@ -1,19 +1,39 @@
+import json 
+
 from flask import request
 from flask import jsonify
 
 from app import app
 from app.tools.whoscrapped import WhoScrapped
+from app.tools.spotify import Spotify
 
-
-@app.route('/sample/', methods=['POST'])
-def search_samples():
-    content = request.json
-    track = content['track']
-    
+def get_samples(track, token):
     scrapper = WhoScrapped()
+    spotify = Spotify(token)
+
     track_path = scrapper.search_track(track)
     samples = scrapper.search_samples(track_path)
+    spotify_track_data = spotify.search_track(track)
+    spotify_sample_data = []
 
-    return jsonify(samples)
+    for sample in samples:
+        spotify_data = spotify.search_track(sample['sample'])
+        spotify_sample_data.append(spotify_data)
+
+    result = {
+        'track': spotify_track_data,
+        'samples': spotify_sample_data
+    }
+    return jsonify(result)
+    
+
+@app.route('/samples/', methods=['POST'])
+def search_samples():
+    token = request.headers.get('X-Spotify')
+    content = request.json
+    track = content['track']
+    samples = get_samples(track, token)
+
+    return samples
 
 
