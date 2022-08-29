4e6858ec145a03f957cccb8362cc86e435009a6d
Draugelis <github@pysc.eu>
2022-08-30 02:46:57 +0300

    add get track

---
+++ app/tools/spotify/get_track.py
@@ -0,0 +1,34 @@
+import requests
+from app.tools.logger import setup_logger
+from app.tools.spotify.config import base_url
+from app.tools.spotify.helpers import get_headers, parse_track
+
+
+def get_track(track_id, token):
+    """Function for getting Spotify track data
+
+    Args:
+        track_id (str): Spotify track id
+        token (str): Spotify auth token
+
+    Raises:
+        RuntimeError: Raised if HTTP request fails
+
+    Returns:
+        dict: Track data
+    """
+    logger = setup_logger(__name__)
+    logger.debug(f'Getting track {track_id}')
+
+    track_url = base_url + '/tracks/' + track_id
+    headers = get_headers(token)
+    response = requests.get(track_url, headers=headers)
+
+    if not response.ok:
+        logger.error(f'Failed to get track {track_id}')
+        raise RuntimeError('Failed to get track')
+
+    logger.info(f'Got track {track_id}')
+    track_data = parse_track(response.json())
+
+    return track_data
