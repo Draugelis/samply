192dfc4fb246c337658659bdcbff133d29273e30
Draugelis <github@pysc.eu>
2022-09-10 01:17:49 +0300

    add url generator

--- app/tools/spotify/helpers.py
+++ app/tools/spotify/helpers.py
@@ -52,3 +52,6 @@ def get_track_id(query):
     if re_track_id:
         return re_track_id.groups()[0]
     return False
+
+def get_playlist_url(playlist_id):
+    return f'https://open.spotify.com/playlist/{playlist_id}'
