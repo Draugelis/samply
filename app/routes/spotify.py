4c66da53cf364c3dc4997b2568635c025ae311c5
Draugelis <github@pysc.eu>
2022-10-06 00:43:30 +0300

    change login handling

--- app/routes/spotify.py
+++ app/routes/spotify.py
@@ -1,16 +1,7 @@
 from app import app
-from flask import make_response, request, redirect
+from flask import render_template
 
 
 @app.route('/spotify', methods=['GET'])
 def spotify_callback():
-    token = request.args.get('access_token')
-    max_token_age = request.args.get('expires_in')
-
-    if not token:
-        return
-
-    response = make_response((redirect('/', code=302)))
-    response.set_cookie('X-Spotify', value=token, max_age=max_token_age)
-
-    return response
+    return render_template('spotify.html')
